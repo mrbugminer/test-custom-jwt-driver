74b9e084facf4a0f85e1f282b173f5afa71abab9
MrBugMiner <mrbugminer@gmail.com>
2024-03-04 03:03:22 +0330

    db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::query()->create([
+            'name' => 'User 1',
+            'email' => '1@1.com',
+            'password' => Hash::make('111111'),
+        ]);
+        User::query()->create([
+            'name' => 'User 2',
+            'email' => '2@2.com',
+            'password' => Hash::make('222222'),
+        ]);
+        User::query()->create([
+            'name' => 'User 3',
+            'email' => '3@3.com',
+            'password' => Hash::make('333333'),
+        ]);
     }
 }
